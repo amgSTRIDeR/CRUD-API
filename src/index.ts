d0422cadc7f52558b2a917c3cfc45375a4a74c34
amgstrider <amgstrider@gmail.com>
2024-02-12 16:28:33 +0400

    feat: handle server errors

--- src/index.ts
+++ src/index.ts
@@ -10,68 +10,72 @@ import isProperUser from './utility/isProperUser';
 dotenv.config();
 
 const server = http.createServer((req, res) => {
-  const method = req.method;
-  const urlPath = req.url || '';
-  const urlPathArr = urlPath.split('/') || '';
-  const userId = urlPathArr[3];
-  if (urlPathArr[1] !== 'api' || urlPathArr[2] !== 'users' || urlPathArr[4]) {
-    console.log('url false');
-    return sendResponse(res);
-  }
-
-  if (userId && !checkUuid(userId)) {
-    console.log('user id or after user id false');
-    return sendResponse(
-      res,
-      400,
-      `userID: ${userId} is invalid, should be in UUID format`
-    );
-  }
-
-  switch (method) {
-    case 'GET':
-      handleGet(res, userId);
-      break;
-    case 'POST':
-      let postBody = '';
-      req.on('data', (chunk) => {
-        postBody += chunk;
-      });
-
-      req.on('end', () => {
-        try {
-          const user = JSON.parse(postBody);
-          isProperUser(user)
-            ? handlePost(res, user)
-            : sendResponse(res, 400, 'Wrong properties for user');
-        } catch {
-          sendResponse(res, 400, 'Request body is wrong');
+    try {
+        const method = req.method;
+        const urlPath = req.url || '';
+        const urlPathArr = urlPath.split('/') || '';
+        const userId = urlPathArr[3];
+        if (urlPathArr[1] !== 'api' || urlPathArr[2] !== 'users' || urlPathArr[4]) {
+          console.log('url false');
+          return sendResponse(res);
         }
-      });
-      break;
-    case 'PUT':
-      let putBody = '';
-      req.on('data', (chunk) => {
-        putBody += chunk;
-      });
-
-      req.on('end', () => {
-        try {
-          const user = JSON.parse(putBody);
-          isProperUser(user)
-            ? handlePut(res, user, userId)
-            : sendResponse(res, 400, 'Wrong properties for user');
-        } catch {
-          sendResponse(res, 400, 'Request body is wrong');
+      
+        if (userId && !checkUuid(userId)) {
+          console.log('user id or after user id false');
+          return sendResponse(
+            res,
+            400,
+            `userID: ${userId} is invalid, should be in UUID format`
+          );
         }
-      });
-      break;
-    case 'DELETE':
-      handleDelete(res, userId);
-      break;
-    default:
-      sendResponse(res);
-  }
+      
+        switch (method) {
+          case 'GET':
+            handleGet(res, userId);
+            break;
+          case 'POST':
+            let postBody = '';
+            req.on('data', (chunk) => {
+              postBody += chunk;
+            });
+      
+            req.on('end', () => {
+              try {
+                  const user = JSON.parse(postBody);
+                  isProperUser(user)
+                  ? handlePost(res, user)
+                  : sendResponse(res, 400, 'Wrong properties for user');
+                } catch {
+                    sendResponse(res, 400, 'Request body is wrong');
+              }
+            });
+            break;
+          case 'PUT':
+            let putBody = '';
+            req.on('data', (chunk) => {
+              putBody += chunk;
+            });
+      
+            req.on('end', () => {
+              try {
+                const user = JSON.parse(putBody);
+                isProperUser(user)
+                  ? handlePut(res, user, userId)
+                  : sendResponse(res, 400, 'Wrong properties for user');
+              } catch {
+                sendResponse(res, 400, 'Request body is wrong');
+              }
+            });
+            break;
+          case 'DELETE':
+            handleDelete(res, userId);
+            break;
+          default:
+            sendResponse(res);
+        }
+    } catch {
+        sendResponse(res, 500, `server error`)
+    }
 });
 
 server.listen(process.env.PORT || 4000);
